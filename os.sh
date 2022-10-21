247ee13130d7ab73ac678309b506a15ed7dd6ee9
sushi-chaaaan <sushi_code@outlook.jp>
2022-10-21 19:03:59 +0900

    delete shell scripts

--- os.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ "$(uname)" = "Darwin" ]; then
-    echo "macOS"
-elif [[ "$(uname -r)" = *"WSL"* ]]; then
-    echo "WSL"
-elif [ "$(uname)" = "Linux" ]; then
-    echo "Linux"
-fi
